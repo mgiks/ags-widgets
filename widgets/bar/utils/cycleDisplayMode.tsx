14a48b57ab073bd79580dd81badb651e8341aa87
mgiks <lidamil6785@gmail.com>
2025-05-25 21:21:07 +0600

    fix: make cycling work as intended

--- widgets/bar/utils/cycleDisplayMode.tsx
+++ widgets/bar/utils/cycleDisplayMode.tsx
@@ -1,9 +1,19 @@
-import { Variable, writeFile } from 'astal'
+import { readFile, Variable, writeFile } from 'astal'
 
-const currentDisplayModeIndex = Variable(0)
+const displayModes = ['years', 'days', 'hours', 'minutes', 'seconds']
+const displayModeToIndex = displayModes.reduce((acc, cur, i) => {
+  acc[cur] = i
+  return acc
+}, {} as { [key: string]: number })
+
+let initialDisplayMode = readFile(
+  '/home/mgik/.config/ags/widgets/bar/.cache/display-mode',
+)
+if (!initialDisplayMode) initialDisplayMode = 'days'
+
+const currentDisplayModeIndex = Variable(displayModeToIndex[initialDisplayMode])
 
 export function cycleDisplayMode() {
-  const displayModes = ['days', 'hours', 'minutes', 'seconds', 'years']
   if (currentDisplayModeIndex.get() < displayModes.length - 1) {
     currentDisplayModeIndex.set(currentDisplayModeIndex.get() + 1)
   } else {
