076e9757ec64967ecf30daba8ac9bd460a0ea632
mgiks <lidamil6785@gmail.com>
2025-09-17 21:22:18 +0600

    feat: add bluetooth panel

---
+++ widgets/bar/BluetoothPanel.tsx
@@ -0,0 +1,40 @@
+import Bluetooth from 'gi://AstalBluetooth'
+import { Gtk } from 'ags/gtk4'
+import { createBinding, createComputed } from 'ags'
+
+function BluetoothPanel() {
+  const bluetooth = Bluetooth.get_default()
+
+  const connected = createBinding(bluetooth, 'isConnected')
+
+  const deviceName = createComputed([connected], () => {
+    for (const device of bluetooth.get_devices()) {
+      if (device.connected) {
+        return device.name
+      }
+    }
+    return ''
+  })
+
+  return (
+    <box
+      valign={Gtk.Align.CENTER}
+      halign={Gtk.Align.CENTER}
+      cssClasses={['panel']}
+    >
+      <box spacing={3}>
+        <image
+          icon_name={deviceName.as((devicName) =>
+            devicName ? 'bluetooth-connected' : 'bluetooth-disconnected-custom'
+          )}
+        />
+        <label
+          visible={deviceName.as((deviceName) => deviceName.length > 0)}
+          label={deviceName}
+        />
+      </box>
+    </box>
+  )
+}
+
+export default BluetoothPanel
