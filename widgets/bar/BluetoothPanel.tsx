87ded923e87a77e4c4d71b3f37d45afdfcc406a3
mgiks <lidamil6785@gmail.com>
2025-10-21 01:16:01 +0600

    fix: prevent '-100%' battery percentage

--- widgets/bar/BluetoothPanel.tsx
+++ widgets/bar/BluetoothPanel.tsx
@@ -1,38 +1,51 @@
 import Bluetooth from 'gi://AstalBluetooth'
 import { Gtk } from 'ags/gtk4'
-import { createBinding, createComputed } from 'ags'
+import { Accessor, createBinding } from 'ags'
+import { createPoll } from 'ags/time'
+
+function findConnectedDevices(devices: Bluetooth.Device[]) {
+  for (const device of devices) {
+    if (device.connected) {
+      return device
+    }
+  }
+  return devices[0]
+}
 
 function BluetoothPanel() {
   const bluetooth = Bluetooth.get_default()
 
   const connected = createBinding(bluetooth, 'isConnected')
 
-  const deviceName = createComputed([connected], () => {
-    for (const device of bluetooth.get_devices()) {
-      if (device.connected) {
-        return device.name + ' ' + (device.batteryPercentage * 100).toString() +
-          '%'
-      }
-    }
-    return ''
-  })
+  const batteryPercentage = createPoll('', 1000, () => (
+    (findConnectedDevices(bluetooth.get_devices()).batteryPercentage * 100)
+      .toString() + '%'
+  ))
+
+  const deviceName = createPoll('', 1000, () => (
+    findConnectedDevices(bluetooth.get_devices()).name
+  ))
 
   return (
     <box
       valign={Gtk.Align.CENTER}
       halign={Gtk.Align.CENTER}
       cssClasses={['panel']}
+      visible={connected}
     >
       <box spacing={3}>
         <image
-          icon_name={deviceName.as((devicName) =>
-            devicName ? 'bluetooth-connected' : 'bluetooth-disconnected-custom'
-          )}
+          icon_name={'bluetooth-connected'}
         />
-        <label
-          visible={deviceName.as((deviceName) => deviceName.length > 0)}
-          label={deviceName}
-        />
+        <box spacing={6}>
+          <label
+            label={deviceName}
+          />
+          <label
+            label={batteryPercentage}
+            visible={batteryPercentage.as((perc) => perc !== '-100%')}
+          />
+        </box>
       </box>
     </box>
   )
