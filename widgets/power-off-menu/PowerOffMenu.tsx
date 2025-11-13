3242f809786b860c062922dc83bcef2fca672df2
mgiks <lidamil6785@gmail.com>
2025-11-13 20:18:36 +0600

    feat: change style of power-off menu

--- widgets/power-off-menu/PowerOffMenu.tsx
+++ widgets/power-off-menu/PowerOffMenu.tsx
@@ -1,4 +1,4 @@
-import { Accessor } from 'ags'
+import { Accessor, createState } from 'ags'
 import { Astal, Gdk, Gtk } from 'ags/gtk4'
 import app from 'ags/gtk4/app'
 import { exec } from 'ags/process'
@@ -8,15 +8,19 @@ export const POWER_OFF_MENU_WINDOW_NAME = 'power-off-menu'
 export default function PowerOffMenu(
   { gdkmonitor }: { gdkmonitor: Gdk.Monitor },
 ) {
+  const [powerOffType, setPowerOffType] = createState('Shutdown')
+  const { TOP, LEFT, RIGHT, BOTTOM } = Astal.WindowAnchor
+
   return (
     <window
       name={POWER_OFF_MENU_WINDOW_NAME}
       cssClasses={['widget']}
+      anchor={TOP | LEFT | RIGHT | BOTTOM}
       gdkmonitor={gdkmonitor}
       application={app}
       exclusivity={Astal.Exclusivity.EXCLUSIVE}
       keymode={Astal.Keymode.EXCLUSIVE}
-      resizable={false}
+      resizable={true}
     >
       <Gtk.EventControllerKey
         onKeyPressed={({ widget }, keyval) => {
@@ -24,43 +28,48 @@ export default function PowerOffMenu(
             case (Gdk.KEY_Escape):
               widget.hide()
               break
+            case (Gdk.KEY_h):
             case (Gdk.KEY_j):
               widget.child_focus(Gtk.DirectionType.TAB_FORWARD)
               break
+            case (Gdk.KEY_l):
             case (Gdk.KEY_k):
               widget.child_focus(Gtk.DirectionType.TAB_BACKWARD)
               break
           }
         }}
       />
-      <box cssClasses={['power-off-menu']}>
-        <box orientation={Gtk.Orientation.VERTICAL}>
-          <box
-            orientation={Gtk.Orientation.HORIZONTAL}
-          >
+      <box
+        cssClasses={['power-off-menu']}
+        orientation={Gtk.Orientation.VERTICAL}
+      >
+        <box
+          orientation={Gtk.Orientation.VERTICAL}
+          halign={Gtk.Align.CENTER}
+          valign={Gtk.Align.CENTER}
+          hexpand={true}
+          vexpand={true}
+        >
+          <label
+            cssClasses={['power-off-menu__label']}
+            label={powerOffType}
+          />
+          <box>
             <button
+              onNotifyHasFocus={() => setPowerOffType('Shutdown')}
+              valign={Gtk.Align.CENTER}
               onClicked={() => turnOffPc()}
               cssClasses={['power-off-menu__button']}
             >
-              <box>
-                <box cssClasses={['power-off-menu__icon']}>
-                  <image iconName='power-off' />
-                </box>
-                <label label={'Shutdown'} />
-              </box>
+              <image iconName='power-off' />
             </button>
-          </box>
-          <box orientation={Gtk.Orientation.HORIZONTAL}>
             <button
+              onNotifyHasFocus={() => setPowerOffType('Reboot')}
+              valign={Gtk.Align.CENTER}
               onClicked={() => rebootPc()}
               cssClasses={['power-off-menu__button']}
             >
-              <box>
-                <box cssClasses={['power-off-menu__icon']}>
-                  <image iconName='reboot' />
-                </box>
-                <label label={'Restart'} />
-              </box>
+              <image iconName='reboot' />
             </button>
           </box>
         </box>
